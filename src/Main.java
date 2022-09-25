2109b874c00cba2e1d9c048f70bbeeb9f12f84c9
Волков Егор <egor.volkov20038@gmail.com>
2022-09-25 17:12:00 +0300

    одну секундочку

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Students students = new Students();
         students.setArray(true);
+        System.out.println("Неотсортированный список:");
         students.outArray();
-        //students.mergeSort();
+        students.quickSort();   // Сортировка быстрой сортировкой
+        System.out.println("Отсортированный список:");
         students.outArray();
+
+        students.setArray(true);
+        System.out.println("Неотсортированный список:");
+        students.outArray();
+        students.mergeSort();   // Сортировка слиянием
+        System.out.println("Отсортированный список:");
+        students.outArray();
+
+        ArrayList<Student> students1 = new ArrayList<>();
+        ArrayList<Student> students2 = new ArrayList<>();   // Создали 2 списка студентов
+        Students.setArray(students1, 5);
+        Students.setArray(students2, 5);    // "Наполнили" 2 списка студентов
+        Students.outArray(students1);
+        Students.outArray(students2);   // "Вывели" 2 списка студентов
+        Students.mergeSort(students1, students2);
     }
 }
--- src/Students.java
+++ src/Students.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Students {
     private ArrayList<Student> iDNumber = new ArrayList<>();
@@ -10,8 +8,74 @@ public class Students {
         iDNumber.sort(sortingStudentsByGPAComparator);
     }
 
+    public static Student[] mergeSort(Student[] a, int n) {
+        if (n < 2) {
+            return a;
+        }
+        if (a == null) return null;
+        int mid = n / 2;
+        Student[] l = new Student[mid];
+        Student[] r = new Student[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        return merge(a, l, r, mid, n - mid);
+    }
+    public static Student[] mergeSort(Student[] a, Student[] b) {
+        int n = a.length + b.length;
+        Student[] c = new Student[n];
+        int i;
+        for (i = 0; i < a.length + b.length; i++)
+            if (i < a.length) c[i] = a[i];
+            else c[i] = b[i- a.length];
+        return mergeSort(c, n);
+    }
+    public static ArrayList<Student> mergeSort(ArrayList<Student> a, ArrayList<Student> b) {
+        int n = a.size() + b.size();
+        Student[] c = new Student[n];
+        for (int i = 0; i < a.size() + b.size(); i++)
+            if (i < a.size()) c[i] = a.get(i);
+            else c.add(b.get(i - a.size()));
+
+
+    }
+    public static Student[] merge(
+            Student[] a, Student[] l, Student[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i].getGPA() >= r[j].getGPA()) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+        return a;
+    }
+    public void mergeSort() {
+        Student[] students = new Student[iDNumber.size()];
+        for (int i = 0; i < iDNumber.size(); i++)
+            students[i] = iDNumber.get(i);
+        Students.mergeSort(students, students.length);
+        Collections.addAll(iDNumber, students);
+    }
 
     public void setArray() {
+        iDNumber = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество студентов: ");
         int n = scanner.nextInt();
@@ -54,8 +118,26 @@ public class Students {
         }
         System.out.println("Студенты успешно добавлены!");
     }
+    public static ArrayList<Student> setArray(ArrayList<Student> iDNumber, int amount) {
+        Random random = new Random();
+        for (int i = 0; i < amount; i++) {
+            String name = "Иван" + i;
+            String curName = "Смирнов";
+            String specialization = "Киб";
+            int courseNumber = random.nextInt(4)+1;
+            int groupNumber = random.nextInt(12)+1;
+            double GPA = random.nextDouble()*4+1;
+            iDNumber.add(new Student(name, curName, specialization, courseNumber, groupNumber, GPA));
+        }
+        return iDNumber;
+    }
 
     public void outArray() {
+        for (Student student : iDNumber) {
+            System.out.println(student);
+        }
+    }
+    public static void outArray(ArrayList<Student> iDNumber) {
         for (Student student : iDNumber) {
             System.out.println(student);
         }
