15a66bc82ea852c1d0a3626f29143ac73c0ba2f5
Волков Егор <egor.volkov20038@gmail.com>
2022-09-26 10:53:05 +0300

    80% пацаны

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.time.Clock;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //task1();
-        task2();
+        //task2();
+        //task3();
+        //task4();
+        task5();
     }
     public static void task1() {
         System.out.println("Волков\nДата и время получения задания:\n12:40 10 сентября 2022 года\nТекущая дата: ");
@@ -50,5 +55,35 @@ public class Main {
         System.out.println("Разница составляет:\nДней: " + (difference.get(GregorianCalendar.DAY_OF_MONTH)-1) +
                 "\nМесяцев: " + difference.get(GregorianCalendar.MONTH) +
                 "\nЛет: " + (difference.get(GregorianCalendar.YEAR)-1970));
+    }
+    public static void task3() {
+        Student student = new Student("Мартышка", "Мартышковна", "специалист по мартышкам",
+                2, 3, 3.5);
+        System.out.println(student);
+    }
+    public static void task4() {
+        Scanner inp = new Scanner(System.in);
+        int year, month, day, hour, minute;
+        System.out.print("Введите год: ");
+        year = inp.nextInt();
+        System.out.print("Введите месяц: ");
+        month = inp.nextInt() - 1;
+        System.out.print("Введите день: ");
+        day = inp.nextInt();
+        System.out.print("Введите час: ");
+        hour = inp.nextInt();
+        System.out.print("Введите минуту: ");
+        minute = inp.nextInt();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day ,hour, minute);
+        Date date = calendar.getTime();
+        System.out.print("Класс Calendar: " + calendar + "\nКласс Date:" + date);
+    }
+    public void task5() {
+        LinkedList<Student> studentsList = new LinkedList<>();
+        ArrayList<Student> studentsArray = new ArrayList<>();
+
+        long beginTime = System.currentTimeMillis();
+        for ()
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,18 @@
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Student {
     private String name, curName, specialization;
     private int courseNumber, groupNumber;
     private double GPA;
     private GregorianCalendar birthday;
+    private SimpleDateFormat sdf;
 
     public Student(String name, String curName, String specialization, int courseNumber, int groupNumber, double GPA,
                    GregorianCalendar birthday) {
@@ -16,14 +24,37 @@ public class Student {
         this.GPA = GPA;
         this.birthday = birthday;
     }
-    public Student(String name, String curName, String specialization, int courseNumber, int groupNumber, double GPA) {
+    public Student(String name, String curName, String specialization, int courseNumber, int groupNumber, double GPA){
+        Scanner inp = new Scanner(System.in);
         this.name = name;
         this.curName = curName;
         this.specialization = specialization;
         this.courseNumber = courseNumber;
         this.groupNumber = groupNumber;
         this.GPA = GPA;
+
+        System.out.println("Введите дату рождения студента: ");
+
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd MM yyyy", new Locale("ru"));
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd MMMM yyyy", new Locale("ru"));
+        Date date = new Date();
         this.birthday = new GregorianCalendar();
+            while (true) {
+            String userDate = inp.nextLine();
+            try {
+                date = sdf1.parse(userDate);
+                this.sdf = sdf1;
+                break;
+            } catch (ParseException ignored) {}
+            try {
+                date = sdf2.parse(userDate);
+                this.sdf = sdf2;
+                break;
+            } catch (ParseException e) {
+                System.out.print("Некорректный формат даты! Попробуйте еще раз: ");
+            }
+        }
+        this.birthday.setTime(date);
     }
 
 
@@ -83,10 +114,19 @@ public class Student {
     public void setBirthday(GregorianCalendar birthday) {
         this.birthday = birthday;
     }
+    public void setBirthday() {
+        Scanner inp = new Scanner(System.in);
+        int day, month, year;
+        this.birthday.set(GregorianCalendar.DAY_OF_MONTH, inp.nextInt());
+        this.birthday.set(GregorianCalendar.MONTH, inp.nextInt()-1);
+        this.birthday.set(GregorianCalendar.YEAR, inp.nextInt());
+
+    }
 
     @Override
     public String toString() {
-
+        Date temp = new Date(birthday.getTimeInMillis());
+        String formattedDate = this.sdf.format(temp);
         return String.format("Student{" +
                 "name='" + name + '\'' +
                 ", curName='" + curName + '\'' +
@@ -94,6 +134,7 @@ public class Student {
                 ", courseNumber=" + courseNumber +
                 ", groupNumber=" + groupNumber +
                 ", GPA=%.2f" +
+                ", birthday=" + formattedDate +
                 '}', GPA);
     }
 }
