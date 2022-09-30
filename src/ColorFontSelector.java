315c8559508498f95d071c51c320b092c04d3c3b
Волков Егор <egor.volkov20038@gmail.com>
2022-10-01 02:11:23 +0300

    FINAL

--- src/ColorFontSelector.java
+++ src/ColorFontSelector.java
@@ -1,52 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ColorFontSelector {
     private JFrame frame;
+    private final Font twr = new Font("Times New Roman", Font.PLAIN, 24);
+    private final Font mss = new Font("MS Sans Serif", Font.PLAIN, 24);
+    private final Font cn = new Font("Courier New", Font.PLAIN, 24);
     JTextArea textArea;
     ColorFontSelector() {
         JMenuBar menuBar = new JMenuBar();
-        createColorMenu();
+        createColorsMenu();
         // Добавление в главное меню выпадающих пунктов меню
-        menuBar.add(createColorMenu());
-        menuBar.add(createEditMenu());
+        menuBar.add(createColorsMenu());
+        menuBar.add(createFontMenu());
+
         frame = new JFrame();
         textArea = new JTextArea();
         textArea.setBounds(0, 0, 100, 100);
-        frame.add(menuBar);
+        textArea.setFont(twr);
+        frame.setJMenuBar(menuBar);
         frame.add(textArea);
         frame.setSize(3000, 3000);
         frame.setVisible(true);
     }
-    private JMenu createColorMenu() {
+    private JMenu createColorsMenu() {
         JMenu fileMenu = new JMenu("Файл");
-        JMenuItem menuItemBlackColor = new JMenuItem("Чёрный");
-        menuItemBlackColor.addActionListener(new ActionListener() {
-            @Override
+        JMenuItem menuItemBlack = new JMenuItem(new AbstractAction("Чёрный") {
             public void actionPerformed(ActionEvent e) {
-                textArea.select(0, textArea.getText().length()-1);
-                textArea.setSelectedTextColor(Color.green);
+                textArea.setForeground(Color.BLACK);
             }
         });
-        JMenuItem menuItemBlueColor = new JMenuItem("Синий");
-        JMenuItem menuItemRedColor = new JMenuItem("Красный");
+        JMenuItem menuItemBlue = new JMenuItem(new AbstractAction("Синий") {
+            public void actionPerformed(ActionEvent e) {
+                textArea.setForeground(Color.BLUE);
+            }
+        });
+        JMenuItem menuItemRed = new JMenuItem(new AbstractAction("Красный") {
+            public void actionPerformed(ActionEvent e) {
+                textArea.setForeground(Color.RED);
+            }
+        });
 
-       // fileMenu.add(menuItemSave);
-        //fileMenu.add(menuItemExit);
+
+        fileMenu.add(menuItemBlack);
+        fileMenu.add(menuItemBlue);
+        fileMenu.add(menuItemRed);
 
         return fileMenu;
     }
-    private JMenu createEditMenu() {
-        JMenu editMenu = new JMenu("Правка");
-        JMenuItem menuItemCopy = new JMenuItem("Копировать");
-        JMenuItem menuItemCut = new JMenuItem("Вырезать");
-        JMenuItem menuItemPaste = new JMenuItem("Вставить");
+    private JMenu createFontMenu() {
+        JMenu editMenu = new JMenu("Шрифт");
 
-        editMenu.add(menuItemCopy);
-        editMenu.add(menuItemCut);
-        editMenu.add(menuItemPaste);
+        JMenuItem menuItemTWR = new JMenuItem(new AbstractAction("Times New Roman") {
+            public void actionPerformed(ActionEvent e) {
+                textArea.setFont(twr);
+            }
+        });
+        JMenuItem menuItemMSS = new JMenuItem(new AbstractAction("MS Sans Serif") {
+            public void actionPerformed(ActionEvent e) {
+                textArea.setFont(mss);
+            }
+        });
+        JMenuItem menuItemCN = new JMenuItem(new AbstractAction("Courier New") {
+            public void actionPerformed(ActionEvent e) {
+                textArea.setFont(cn);
+            }
+        });
+        JMenuItem menuItemPaste = new JMenuItem("Courier New");
+
+        editMenu.add(menuItemTWR);
+        editMenu.add(menuItemMSS);
+        editMenu.add(menuItemCN);
         return editMenu;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,3 @@ public class Main {
         ColorFontSelector colorFontSelector = new ColorFontSelector();
     }
 }
-/*
- public BorderLayoutTest()
-    {
-        super("BorderLayoutTest");
-        setDefaultCloseOperation( EXIT_ON_CLOSE );
-        setSize(250, 250);
-        // Панель содержимого
-        Container container = getContentPane();
-        container.add(new JButton("Север" ), "North");
-                container.add(new JButton("Юг"    ), "South");
-                container.add(new JLabel ("Запад" ), BorderLayout.WEST);
-                container.add(new JLabel ("Восток"), BorderLayout.EAST);
-                // При отсутствии 2-го параметра компонент размещается в центре
-                container.add(new JButton("Центр"));
-                // Открываем окно
-                setVisible(true);
-                }
- */
