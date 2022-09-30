5d6d34591f7304a4815a85d0960fb5aba7667f53
Волков Егор <egor.volkov20038@gmail.com>
2022-10-01 01:44:40 +0300

    t

--- src/ColorFontSelector.java
+++ src/ColorFontSelector.java
@@ -1,16 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ColorFontSelector {
     private JFrame frame;
+    JTextArea textArea;
     ColorFontSelector() {
+        JMenuBar menuBar = new JMenuBar();
+        createColorMenu();
+        // Добавление в главное меню выпадающих пунктов меню
+        menuBar.add(createColorMenu());
+        menuBar.add(createEditMenu());
         frame = new JFrame();
-        JTextArea textArea = new JTextArea();
-        textArea.setSelectionColor(Color.green);
+        textArea = new JTextArea();
         textArea.setBounds(0, 0, 100, 100);
+        frame.add(menuBar);
         frame.add(textArea);
         frame.setSize(3000, 3000);
         frame.setVisible(true);
+    }
+    private JMenu createColorMenu() {
+        JMenu fileMenu = new JMenu("Файл");
+        JMenuItem menuItemBlackColor = new JMenuItem("Чёрный");
+        menuItemBlackColor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.select(0, textArea.getText().length()-1);
+                textArea.setSelectedTextColor(Color.green);
+            }
+        });
+        JMenuItem menuItemBlueColor = new JMenuItem("Синий");
+        JMenuItem menuItemRedColor = new JMenuItem("Красный");
+
+       // fileMenu.add(menuItemSave);
+        //fileMenu.add(menuItemExit);
+
+        return fileMenu;
+    }
+    private JMenu createEditMenu() {
+        JMenu editMenu = new JMenu("Правка");
+        JMenuItem menuItemCopy = new JMenuItem("Копировать");
+        JMenuItem menuItemCut = new JMenuItem("Вырезать");
+        JMenuItem menuItemPaste = new JMenuItem("Вставить");
+
+        editMenu.add(menuItemCopy);
+        editMenu.add(menuItemCut);
+        editMenu.add(menuItemPaste);
+        return editMenu;
+    }
+
+    class ExitAction extends AbstractAction {
+        ExitAction() {
+            putValue(NAME, "Выход");
+        }
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
     }
 
 }
