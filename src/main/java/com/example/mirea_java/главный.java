b97ad0c78d14b7545da7327aab85b9af95a5565b
Волков Егор <egor.volkov20038@gmail.com>
2022-10-01 03:23:48 +0300

    FINAL

---
+++ src/main/java/com/example/mirea_java/главный.java
@@ -0,0 +1,23 @@
+package com.example.mirea_java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class главный extends Application {
+
+    @Override
+    public void start(Stage компонент) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(главный.class.getResource("Application.fxml"));
+        Scene сцена = new Scene(fxmlLoader.load());
+        компонент.setResizable(false);
+        компонент.setTitle("Дикий блокнот");
+        компонент.setScene(сцена);
+        компонент.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
